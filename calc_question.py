c78839103a5d18b6e64842c4dfef605f64bc57a2
Thomas Barrett <trbarrettjr@users.noreply.github.com>
2025-02-18 11:12:40 -0800

    Add files via upload
    Added more code.

--- calc_question.py
+++ calc_question.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
+import math
+
 a = [1/x for x in range(1, 101)]
 
-csv_out = []
+csv_out = ['float,denominator,numerator,base2 denominator,base2 numerator\n']
 
 for val in a:
-    csv_out.append(f'{val},{val.as_integer_ratio()[0]},{val.as_integer_ratio()[1]}\n')
+    csv_out.append(f'{val},'\
+                   f'{val.as_integer_ratio()[0]},'\
+                   f'{val.as_integer_ratio()[1]},'\
+                   f'{math.log2(val.as_integer_ratio()[0])},'\
+                   f'{math.log2(val.as_integer_ratio()[1])}\n')
 
 
 with open('values.csv', 'w') as f:
